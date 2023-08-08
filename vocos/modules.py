8edc19c33c0c4967b4831293415b513db908d8c8
lmaxwell <shysian@gmail.com>
2023-08-08 15:38:32 +0800

    add casual conv1d

--- vocos/modules.py
+++ vocos/modules.py
@@ -4,6 +4,31 @@ import torch
 from torch import nn
 from torch.nn.utils import weight_norm, remove_weight_norm
 
+class CausalConv1d(torch.nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
+        
+        self.__padding = (kernel_size - 1) * dilation
+        
+    def forward(self, input):
+        return super(CausalConv1d, self).forward(F.pad(input, (self.__padding, 0)))
+
 
 class ConvNeXtBlock(nn.Module):
     """ConvNeXt Block adapted from https://github.com/facebookresearch/ConvNeXt to 1D audio signal.
